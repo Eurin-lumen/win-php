51c2471db7f096070ead77ed3fca5ceae3f26deb
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-03-09 01:00:57 +0100

    Nouveau

--- index.php
+++ index.php
@@ -3,13 +3,8 @@
 require 'Personnage.php';
 
 $merlin = new Personnage("Merlin");
-
-$merlin->regenerer();
-
-// nouveau personnages
 $harry = new Personnage("Harry");
-$harry->mort(); // false 
 
-var_dump($merlin); // -> accède à l'élement
 
-//var_dump($harry->mort()); 
+var_dump($merlin);
+var_dump($harry);
